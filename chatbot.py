ac37dfb0a8fc2ae79265704f5931011dd5375af4
aafaq <maafaq.mscs23seecs@seecs.edu.pk>
2025-06-18 19:08:58 +0500

    Messages feature integrated with chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,5 @@
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage,SystemMessage,AIMessage
 from dotenv import load_dotenv
 import os 
 load_dotenv()
@@ -7,15 +8,17 @@ api_key=os.getenv("OPEN_AI_API_KEY")
 model=ChatOpenAI(model="gpt-4o-mini",
                  openai_api_key=api_key)
 
-chat_history=[]
+chat_history=[
+    SystemMessage(content="You are a helpful assistant"),
+]
 while True:
     user_input=input("You: ")
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(content=user_input))
     if user_input=='exit':
         break
     
     result=model.invoke(chat_history)
-    chat_history.append(result)
+    chat_history.append(AIMessage(content=result.content))
     print("AI: ",result.content)
     
 print(chat_history)
